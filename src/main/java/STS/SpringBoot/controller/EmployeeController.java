505dfb842fe6c8716c872f8583efddec041b83cb
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-03-19 16:25:14 +0530

    API Layer

---
+++ src/main/java/STS/SpringBoot/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package STS.SpringBoot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import STS.SpringBoot.model.Employee;
+import STS.SpringBoot.services.EmployeeService;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+	
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+	
+	
+	//Create Employee Rest API
+	@PostMapping
+	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
+		return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+	}
+}
