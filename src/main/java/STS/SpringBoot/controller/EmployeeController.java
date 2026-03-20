477291ddba39d68ec14f61667baa0887b2d9b4a3
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-03-20 20:01:54 +0530

    Add Get Or GetById Method

--- src/main/java/STS/SpringBoot/controller/EmployeeController.java
+++ src/main/java/STS/SpringBoot/controller/EmployeeController.java
@@ -1,7 +1,11 @@
 package STS.SpringBoot.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +26,22 @@ public class EmployeeController {
 	}
 	
 	
-	//Create Employee Rest API
+	//Create Insert Employee Rest API
 	@PostMapping
 	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
 		return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+	}
+	
+	//Create Get (Retrive) All Employee Rest API
+	@GetMapping
+	public List<Employee> getAllEmployees(){
+		return employeeService.getAllEmployees();
+	}
+	
+	//Get Employee By Id
+	//http://localhost:8080/api/employees/1
+	@GetMapping("{Id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable("Id") long employeeId){
+		return new ResponseEntity<Employee>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
 	}
 }
