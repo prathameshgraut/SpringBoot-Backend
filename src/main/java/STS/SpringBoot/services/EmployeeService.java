0bf254bc27f0d3ac80341422d2fb42a700787923
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-03-20 19:55:18 +0530

    Updated Get Method

--- src/main/java/STS/SpringBoot/services/EmployeeService.java
+++ src/main/java/STS/SpringBoot/services/EmployeeService.java
@@ -1,9 +1,17 @@
 package STS.SpringBoot.services;
 
+import java.util.List;
+
 import STS.SpringBoot.model.Employee;
 
 public interface EmployeeService {
 	
+	//Save Employee Data
 	Employee saveEmployee(Employee employee);
 
+	//Get Employee Data To Fetch Multiple Record At Time Then Use To ArrayList
+	List<Employee> getAllEmployees();
+	
+	//Get Employee By Id
+	Employee getEmployeeById(Long Id);
 }
